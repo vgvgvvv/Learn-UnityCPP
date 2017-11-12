88d68d8b2d5e8b57c90b7f650d75b467f6dc8e90
DC <352070977@qq.com>
2017-11-12 16:45:02 +0800

    初次提交

---
+++ SwigLib/src/test/Test.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+
+namespace SwigLibs
+{
+    int TestSwig::Add(int a, int b)
+    {
+        return a + b;
+    }
+};
---
+++ SwigLib/src/test/Test.h
@@ -0,0 +1,13 @@
+#ifndef __TEST__
+#define __TEST__
+
+namespace SwigLibs
+{
+    class TestSwig
+    {
+    public:
+        static int Add(int a, int b);
+    };
+}
+
+#endif
